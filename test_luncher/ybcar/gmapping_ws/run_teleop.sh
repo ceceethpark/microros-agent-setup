3557864ad5d8d5fee46e421f24b2f1a2d6fa2bb4
thpark <ceceethpark@gmail.com>
2025-12-15 09:58:20 +0900

    [main] Remove all files except README.md in each test_luncher subfolder from remote tracking

--- test_luncher/ybcar/gmapping_ws/run_teleop.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-# Run teleop_twist_keyboard in the current terminal to publish cmd_vel
-echo "Sourcing ROS2 Jazzy..."
-source /opt/ros/jazzy/setup.bash
-
-echo "Start teleop (will use your keyboard in this terminal). Ctrl-C to exit."
-exec ros2 run teleop_twist_keyboard teleop_twist_keyboard
